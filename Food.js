a754e6d1f219800f5490bb35f8360ea68efa7ca2
irinsajan <69659076+irinsajan@users.noreply.github.com>
2021-03-26 01:29:36 +0530

    Update Food.js

--- Food.js
+++ Food.js
@@ -1,26 +1,9 @@
 class food{
-    constructor
-        var foodStock
-        var lastFed
-
-        ;
-
+    constructor(){}
+        
     
-    display()
-        var x=80,y=100;
-
-        imageMode(CENTER);
-        image(this.image,720,220,70,70);
-
-        if(this.foodStock!=0){
-            for(var i=0;i<this.foodStock;++){
-                if(i%10==0){
-                    x=80
-                    y=y+50;
-                }
-                image(this.image,x,y,50,50);
-                x=x+30;
-
+    display(){
+        
                 var addFood=createButton("Add Food");
                 addFood.position(500,125);
 
@@ -38,5 +21,6 @@ class food{
                     gameState=1;
                     database.ref('/').update({'gameState':gameState})
                 }
-
+    }
+}
             
